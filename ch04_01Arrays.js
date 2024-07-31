82c970f5fbf428a23d70aa072a268cb0a6c3247e
Trushantkose <trushantkose6@gmail.com>
2024-07-31 17:35:19 +0530

    looping on array

---
+++ ch04_01Arrays.js
@@ -0,0 +1,12 @@
+// Arrays in Js:- Collection items in linear form
+let marks = [98, 88, 90];
+console.log(marks);
+
+// to print array length
+console.log(marks.length); //property gives value but method did some 
+
+//print the heroes name by using arrays
+let heroes = ['iron-man', 'thor', 'hulk', 'captain america', 'krish', 'spider-man']
+console.log(heroes);
+
+// typeof heroes -> object and object is a non-primitive (advance datatype)
---
+++ ch04_02looping_arr.js
@@ -0,0 +1,27 @@
+// Array indices :- the values are stored in linear manner
+let marks = [98, 94, 23, 53, 24];
+for(let i in marks){
+    console.log("i = ", marks[i]);
+}
+
+// looping over an Array:- Print all elements of an array
+let heroes = ['shakti-man', 'krish', 'hulk', 'antman', 'doctor-strange', 'marvels'];
+
+// length property
+// for(let i = 0; i < heroes.length; i++){
+//     console.log("Marvel Hero = ",heroes[i]);
+// }
+
+// for of
+for(let hero of heroes){
+    console.log(hero);
+}
+
+// print the name of cities
+let cities = ['delhi', 'mumbai', 'nagpur', 'pune', 'chennai'];
+for(let city of cities){
+    console.log(city);
+}
+
+
+
